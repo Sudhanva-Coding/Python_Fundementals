60623d56a7117a18c7753f64491a3b92890aaa46
Sudhanva-Coding <sudhanvaakshay7@gmail.com>
2026-04-11 10:51:53 +0100

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,3 @@
+name1 = input("Enter your first name ")
+name2 = input("Enter your last name ")
+print(name2,"s", "first name is", name1)
---
+++ variables.py
@@ -0,0 +1,8 @@
+
+#name = input("What is your name ")
+#print("Good morning", name)
+
+#print("Welcome to JetLearn \n", "Lets learn python")
+
+print("Welcome to Jetlearn", end = "***")
+print("Lets learn python")
